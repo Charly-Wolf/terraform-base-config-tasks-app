5720f28ddaf789e1301d87194f37026c1124d822
Charly <cardan1990@hotmail.com>
2025-06-29 16:51:40 +0200

    delete also all versions and markes in state bucket

--- .github/scripts/destroy-terraform-backend.sh
+++ .github/scripts/destroy-terraform-backend.sh
@@ -5,8 +5,23 @@ REGION="us-east-1"
 BUCKET_NAME="cardp-terraform-state-bucket"
 DYNAMO_TABLE_NAME="terraform-locks"
 
-echo "Deleting all objects from S3 bucket: $BUCKET_NAME..."
-aws s3 rm s3://$BUCKET_NAME --recursive --region $REGION
+echo "Deleting all object versions and delete markers from S3 bucket: $BUCKET_NAME..."
+
+# Delete all object versions
+versions=$(aws s3api list-object-versions --bucket $BUCKET_NAME --region $REGION --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')
+echo $versions | jq -c '.Objects[]?' | while read obj; do
+  key=$(echo $obj | jq -r '.Key')
+  versionId=$(echo $obj | jq -r '.VersionId')
+  aws s3api delete-object --bucket $BUCKET_NAME --key "$key" --version-id "$versionId" --region $REGION
+done
+
+# Delete all delete markers
+markers=$(aws s3api list-object-versions --bucket $BUCKET_NAME --region $REGION --query='{Objects: DeleteMarkers[].{Key:Key,VersionId:VersionId}}')
+echo $markers | jq -c '.Objects[]?' | while read obj; do
+  key=$(echo $obj | jq -r '.Key')
+  versionId=$(echo $obj | jq -r '.VersionId')
+  aws s3api delete-object --bucket $BUCKET_NAME --key "$key" --version-id "$versionId" --region $REGION
+done
 
 echo "Deleting S3 bucket: $BUCKET_NAME..."
 aws s3api delete-bucket --bucket $BUCKET_NAME --region $REGION
